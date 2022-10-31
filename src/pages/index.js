8356b7781bcb018e4ab7d3c21dc9476ebb1c74d9
Frodo <102017064+Frodo-Web@users.noreply.github.com>
2022-10-31 19:31:13 +0300

    Fix API data providing issues at next build

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,6 +22,7 @@ export default HomePage;
 
 export const getStaticProps = async () => {
   const res = await fetch(`${server}/api/articles`);
+  // const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
   const articles = await res.json();
 
   return {
@@ -30,14 +31,3 @@ export const getStaticProps = async () => {
     }
   }
 }
-
-// export const getStaticProps = async () => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
-//   const articles = await res.json();
-
-//   return {
-//     props: {
-//       articles
-//     }
-//   }
-// }
